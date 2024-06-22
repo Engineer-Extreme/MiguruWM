33cc656bfb044ac6a6577e21ea780dcd55117981
imawizard <1701648+imawizard@users.noreply.github.com>
2024-06-22 21:49:07 +0200

    feat(ci): Add manual release workflow (#27)
    * style(ci): Fix some indentation
    
    * chore(ci): Update AutoHotkey version
    
    * feat(ci): Add manual release workflow

--- scripts/embed.ps1
+++ scripts/embed.ps1
@@ -1,31 +1,26 @@
-$ahk2exe = ![string]::IsNullOrEmpty($env:AHK2EXE) `
-  ? $env:AHK2EXE                                `
-  : ".\autohotkey\Compiler\Ahk2Exe.exe"
-
-$base = ![string]::IsNullOrEmpty($env:AHK_STUB) `
-  ? $env:AHK_STUB                             `
-  : ".\autohotkey\AutoHotkey64.exe"
-
 $ico = ".\assets\togepī.ico"
 $ahk = ".\mwm.ahk"
 $out = ".\build"
 
-New-Item -Type Directory $out -Force | Out-Null
+$ahk2exe = ![string]::IsNullOrEmpty($env:AHK2EXE) `
+  ? $env:AHK2EXE                                  `
+  : ".\autohotkey\Compiler\Ahk2Exe.exe"
 
-$date = $(git log -n1 --format='%cI')
-$version = $(if ($date -match '\d\d\d(\d)-(\d\d)-(\d\d)') {
-  $Matches[1]                `
-    + "."                             `
-    + ($Matches[2] -replace "^0", "") `
-    + "."                             `
-    + ($Matches[3] -replace "^0", "") `
-} else { "x.y.z "})
+$base = ![string]::IsNullOrEmpty($env:AHK_STUB) `
+  ? $env:AHK_STUB                               `
+  : ".\autohotkey\AutoHotkey64.exe"
+
+$version = ![string]::IsNullOrEmpty($env:VERSION) `
+  ? $env:VERSION                                  `
+  : "$(git log -n1 --format='%h') (commit hash)"
+
+New-Item -Type Directory $out -Force | Out-Null
 
 (((Get-Content $ahk) -replace '^\s*#include (?:\*i )?(.+)$', {
   "#include $(Resolve-Path $_.Groups[1])"
 }) -match '^\s*#include .+$'), "MIGURU_VERSION := `"$version`""
   | Join-String -Separator `n
-  | Out-File (Join-Path $out "autoload.ahk")
+  | Tee-Object -FilePath (Join-Path $out "autoload.ahk")
 
 Copy-Item $ahk (Join-Path $out "$(Split-Path $ahk -LeafBase).ahk")
 
